cc0adf45aab5ee3fb4700a19be72ff001e4110c1
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-06-15 16:21:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+/ Creating variables
+var team = players;
+var team1 = players.slice(); // Creating a shallow copy of the players array
+var cap1 = Object.assign({}, person); // Creating a shallow copy of the person object
+
 
 window.players = players;
 window.person = person;
