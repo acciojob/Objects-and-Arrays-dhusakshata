bce93c2b6dd05ecfd80ab3420196fc64a726759b
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-06-18 12:58:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const person = {
 };
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 var team=players;
-var team1=Object.assign(players);
+var team1=["John", "Bob", "Alice", "Poppy"];
 
-var cap1=Object.assign(person);
+var cap1={
+  name: "John Doe",
+  age: 80,};
 
 window.players = players;
 window.person = person;
